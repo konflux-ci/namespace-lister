08e0552ab8f6b0e616e297c485f99d46ae5bec9e
Francesco Ilario <filario@redhat.com>
2024-12-03 11:46:31 +0100

    simplify KUBECONFIG in Makefile
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- acceptance/make/common.mk
+++ acceptance/make/common.mk
@@ -11,11 +11,8 @@ GO ?= go
 
 GOLANG_CI ?= $(GO) run -modfile $(shell dirname $(ROOT_DIR))/hack/tools/golang-ci/go.mod github.com/golangci/golangci-lint/cmd/golangci-lint
 
-KUBECONFIG ?=
 KUBECTL ?= kubectl
-KUBECTL_X := KUBECONFIG=$(KUBECONFIG) $(KUBECTL)
 KIND ?= kind
-KIND_X := KUBECONFIG=$(KUBECONFIG) $(KIND)
 KUBECONFIG_ATSA ?= /tmp/namespace-lister-acceptance-tests-user.kcfg
 
 ## Local Folders
@@ -36,32 +33,32 @@ image-build:
 
 .PHONY: kind-create
 kind-create:
-	$(KIND_X) create cluster --name "$(KIND_CLUSTER_NAME)" --config kind-config.yaml
+	$(KIND) create cluster --name "$(KIND_CLUSTER_NAME)" --config kind-config.yaml
 
 .PHONY: kind-load-image
 kind-load-image:
-	$(KIND_X) load docker-image --name "$(KIND_CLUSTER_NAME)" "$(IMG)"
+	$(KIND) load docker-image --name "$(KIND_CLUSTER_NAME)" "$(IMG)"
 
 .PHONY: update-namespace-lister
 update-namespace-lister: image-build load-image
-	$(KUBECTL_X) rollout restart deployment namespace-lister -n namespace-lister
-	$(KUBECTL_X) rollout status deployment -n namespace-lister namespace-lister
+	$(KUBECTL) rollout restart deployment namespace-lister -n namespace-lister
+	$(KUBECTL) rollout status deployment -n namespace-lister namespace-lister
 
 .PHONY: deploy-test-infra
 deploy-test-infra:
-	$(KUBECTL_X) apply -k $(ROOT_DIR)/dependencies/cert-manager/
+	$(KUBECTL) apply -k $(ROOT_DIR)/dependencies/cert-manager/
 	sleep 5
-	$(KUBECTL_X) wait --for=condition=Ready --timeout=300s -l 'app.kubernetes.io/instance=cert-manager' -n cert-manager pod
-	$(KUBECTL_X) apply -k $(ROOT_DIR)/dependencies/cluster-issuer/
+	$(KUBECTL) wait --for=condition=Ready --timeout=300s -l 'app.kubernetes.io/instance=cert-manager' -n cert-manager pod
+	$(KUBECTL) apply -k $(ROOT_DIR)/dependencies/cluster-issuer/
 
 .PHONY: create-test-identity
 create-test-identity:
-	$(KUBECTL_X) apply -k $(ROOT_DIR)/config/acceptance-tests/
+	$(KUBECTL) apply -k $(ROOT_DIR)/config/acceptance-tests/
 
 .PHONY: export-test-identity-kubeconfig
 export-test-identity-kubeconfig:
-	$(KIND_X) get kubeconfig --name $(KIND_CLUSTER_NAME) | \
-		yq '.users[0].user={"token": "'$$($(KUBECTL_X) get secret acceptance-tests-user -n acceptance-tests -o jsonpath='{.data.token}' | base64 -d )'"}' >| \
+	$(KIND) get kubeconfig --name $(KIND_CLUSTER_NAME) | \
+		yq '.users[0].user={"token": "'$$($(KUBECTL) get secret acceptance-tests-user -n acceptance-tests -o jsonpath='{.data.token}' | base64 -d )'"}' >| \
 		$(KUBECONFIG_ATSA)
 
 .PHONY: vet
@@ -70,5 +67,5 @@ vet:
 
 .PHONY: clean
 clean:
-	$(KUBECTL_X) delete namespace -l 'namespace-lister/scope=acceptance-tests'
+	$(KUBECTL) delete namespace -l 'namespace-lister/scope=acceptance-tests'
 
