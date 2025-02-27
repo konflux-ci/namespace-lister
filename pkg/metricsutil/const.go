d00d8e0e45bf549b845691850f17a2d1cac069e0
Francesco Ilario <filario@redhat.com>
2025-02-27 13:01:30 +0100

    add metricsutil package
    Signed-off-by: Francesco Ilario <filario@redhat.com>

---
+++ pkg/metricsutil/const.go
@@ -0,0 +1,9 @@
+package metricsutil
+
+const (
+	ResourcesRequestsMetricFullname     = "namespace_lister_accesscache_resource_requests_total"
+	TimeRequestsMetricFullname          = "namespace_lister_accesscache_time_requests_total"
+	SyncMetricFullname                  = "namespace_lister_accesscache_synch_op_total"
+	SubjectNamespacePairsMetricFullname = "namespace_lister_accesscache_subject_namespace_pairs"
+	SubjectsMetricFullname              = "namespace_lister_accesscache_subjects"
+)
