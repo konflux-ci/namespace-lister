33158f1ba8123c6ece52843ca4b37171a9ceaa66
Francesco Ilario <filario@redhat.com>
2026-05-18 13:48:45 +0200

    fix mock file's name
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- internal/http/middleware/interfaces_test.go
+++ internal/http/middleware/interfaces_test.go
@@ -2,7 +2,7 @@ package middleware_test
 
 import "k8s.io/apiserver/pkg/authentication/authenticator"
 
-//go:generate mockgen -source=interfaces_test.go -destination=mocks/rest_interface.go -package=mocks
+//go:generate mockgen -source=interfaces_test.go -destination=mocks/middleware_interface.go -package=mocks
 
 type Request interface {
 	authenticator.Request
